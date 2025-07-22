aa5744ca4dc3ae40f9b487df2f89d30b066918ed
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 15:17:24 -0300

    refactor: change router to routes folder to improve app architecture

---
+++ src/app/routes/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { CanActivateSession } from '../core/guards/session.guard';
+import { TabsLayoutComponent } from '../layouts/tabs/tabs.component';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('@Features/auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: '',
+    component: TabsLayoutComponent,
+    canActivate: [CanActivateSession],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('@Features/home/home.component').then(m => m.HomeComponent)
+      }
+    ]
+  }
+];
