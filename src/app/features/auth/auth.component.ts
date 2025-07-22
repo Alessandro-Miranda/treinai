20650a01a655976818f78b8bfbeed5cc6dcb36b6
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-21 23:20:43 -0300

    feat: implements basic login + redirect on login

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonButton, IonContent, IonIcon, IonText } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { logoGoogle } from "ionicons/icons";
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ToastComponent } from "src/app/shared/toast/toast.component";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
-  imports: [IonContent, IonButton, IonText, IonIcon],
+  imports: [IonContent, IonButton, IonText, IonIcon, ToastComponent],
 })
 export class AuthComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginToastState = {
+    isOpen: false,
+    message: '',
+    title: ''
+  };
 
   constructor() {
     addIcons({ logoGoogle });
   }
 
+  async handleGoogleLogin() {
+    try {
+      const { user, credential, token } = await this.authService.loginWithGoogle();
+      this.router.navigate(['/home']);
+    } catch (err: any) {
+      this.loginToastState.isOpen = true;
+      this.loginToastState.message = err.message;
+      this.loginToastState.title = 'Erro ao fazer login'
+    }
+  }
 }
