74405846c9d326dbcc085dbdabfa331d4a78fa93
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-21 22:07:52 -0300

    Revert "feat: create service and method to perform login with google"
    tttt reverts commit e30217dde89269a44794fc813c7abbf84cc211e4.

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { IonButton, IonContent, IonIcon, IonText } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { logoGoogle } from "ionicons/icons";
-import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -12,20 +10,9 @@ import { AuthService } from 'src/app/core/services/auth.service';
   imports: [IonContent, IonButton, IonText, IonIcon],
 })
 export class AuthComponent {
-  private authService = inject(AuthService);
-  private router = inject(Router);
 
   constructor() {
     addIcons({ logoGoogle });
-  }
-
-  async handleGoogleLogin() {
-    try {
-      const { user, credential, token } = await this.authService.loginWithGoogle();
-      this.router.navigate(['/home']);
-    } catch(err) {
-      console.error('Erro: ', err);
-    }
   }
 
 }
