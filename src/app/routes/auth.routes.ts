bcd648b477b9232518c59382549accc38b6d6325
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 15:14:48 -0300

    refactor: remove unused router

--- src/app/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { AuthComponent } from "@Features/auth/auth.component";
-
-export const authRoutes: Routes = [
-  {
-    path: '',
-    component: AuthComponent
-  }
-]
