e713693bc48704561030c2e0dda22807e4e65c8f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 22:06:31 -0300

    feat: complete user service responsible for create user on login if not exists

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,30 @@
+import { EnvironmentInjector, inject, Injectable, runInInjectionContext } from "@angular/core";
+import { User } from "@angular/fire/auth";
+import { doc, DocumentData, DocumentReference, Firestore, getDoc, serverTimestamp, setDoc } from "@angular/fire/firestore";
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private firestore = inject(Firestore);
+  private injector = inject(EnvironmentInjector);
+
+  async createIfNotExists(user: User): Promise<void> {
+    let snapshotExists = false;
+    let docRef: DocumentReference<DocumentData, DocumentData>
+
+    await runInInjectionContext(this.injector, async () => {
+      docRef = doc(this.firestore, 'users', user.uid);
+      snapshotExists = (await getDoc(docRef)).exists(); 
+    })
+
+    if (snapshotExists) return;
+
+    await runInInjectionContext(this.injector, async () => {
+      await setDoc(docRef, {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        createdAt: serverTimestamp(),
+      });
+    })
+  }
+}
