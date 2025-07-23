dddeb5f39b4a7e145f0363c1c45f9b51c2d2bc2b
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 22:27:34 -0300

    feat: init training home feature

---
+++ src/app/features/training/training.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './training.component.html',
+  styleUrls: ['./training.component.scss'],
+})
+export class TrainingComponent {
+  constructor() {}
+}
