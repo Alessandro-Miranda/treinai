c76564ef4950468dd094f28062b7cb52d2662a66
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 22:03:57 -0300

    refactor: implements runInInjectionContext to avoid context injector warnings

--- src/app/core/firebase/firebase-auth.service.ts
+++ src/app/core/firebase/firebase-auth.service.ts
@@ -1,20 +1,21 @@
-import { inject, Injectable } from "@angular/core";
-import { Auth, GoogleAuthProvider, signInWithPopup } from "@angular/fire/auth";
+import { EnvironmentInjector, inject, Injectable, runInInjectionContext } from "@angular/core";
+import { Auth, GoogleAuthProvider, signInWithPopup, User } from "@angular/fire/auth";
 
 @Injectable({ providedIn: 'root' })
 export class FirebaseAuthService {
   private auth = inject(Auth);
+  private injector = inject(EnvironmentInjector);
 
-  async signInWithGoogle() {
+  async signInWithGoogle(): Promise<User> {
     const provider = new GoogleAuthProvider();
-    
-    const result = await signInWithPopup(this.auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    
-    return {
-      token: credential?.accessToken,
-      credential,
-      user: result.user,
-    };
+
+    return new Promise((resolve, reject) => {
+      runInInjectionContext(this.injector, async () =>{
+        signInWithPopup(this.auth, provider)
+          .then(({ user }) => {
+            resolve(user);
+          }).catch(err => reject(err));
+      });
+    });
   }
 }
