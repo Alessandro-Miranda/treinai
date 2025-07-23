d40268867943bb11caf304c91341a18df4cc2613
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-22 22:05:01 -0300

    feat: complete navigation after login and user creation if not exists

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { IonButton, IonContent, IonIcon, IonText } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { logoGoogle } from "ionicons/icons";
-import { AuthService } from 'src/app/core/services/auth.service';
+import { FirebaseAuthService } from 'src/app/core/firebase/firebase-auth.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { ToastComponent } from "src/app/shared/toast/toast.component";
 
@@ -14,7 +14,7 @@ import { ToastComponent } from "src/app/shared/toast/toast.component";
   imports: [IonContent, IonButton, IonText, IonIcon, ToastComponent],
 })
 export class AuthComponent {
-  private authService = inject(AuthService);
+  private firebaseService = inject(FirebaseAuthService);
   private router = inject(Router);
   private userService = inject(UserService);
 
@@ -29,10 +29,10 @@ export class AuthComponent {
 
   async handleGoogleLogin() {
     try {
-      const { user } = await this.authService.loginWithGoogle();
+      const user = await this.firebaseService.signInWithGoogle();
 
       await this.userService.createIfNotExists(user);
-      this.router.navigate(['/home']);
+      this.router.navigate(['/home'], { replaceUrl: true });
     } catch (err: any) {
       this.loginToastState.isOpen = true;
       this.loginToastState.message = err.message;
