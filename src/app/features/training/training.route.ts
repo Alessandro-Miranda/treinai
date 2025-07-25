bae67a189e345a84e66a37b8b63df235420a8a51
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-24 21:20:37 -0300

    feat: create router with routes from training feature

--- src/app/features/training/training.route.ts
+++ src/app/features/training/training.route.ts
@@ -1,13 +1,16 @@
-import { Routes } from "@angular/router";
-import { TrainingComponent } from "./pages/home/training.component";
+import { Routes } from '@angular/router';
+import { TrainingComponent } from './pages/home/training.component';
 
 export const trainingRoute: Routes = [
   {
     path: '',
-    component: TrainingComponent
+    component: TrainingComponent,
   },
   {
     path: 'add',
-    loadComponent: () => import('./pages/add-training/add-training.component').then(m => m.AddTrainingComponent)
-  }
-]
+    loadComponent: () =>
+      import('./pages/add-training/add-training.component').then(
+        (m) => m.AddTrainingComponent
+      ),
+  },
+];
