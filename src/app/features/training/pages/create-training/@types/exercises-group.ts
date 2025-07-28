6ef6e66d3c41593339c6a020eaf1faff518713f5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-27 22:46:54 -0300

    feat: create ExercisesGroup type

---
+++ src/app/features/training/pages/create-training/@types/exercises-group.ts
@@ -0,0 +1,13 @@
+import { FormControl } from "@angular/forms";
+
+type ExerciseControl<T> = FormControl<T | null>;
+
+export type ExercisesGroup = {
+  uuid: ExerciseControl<string>;
+  title: ExerciseControl<string>;
+  sets: ExerciseControl<number>;
+  reps: ExerciseControl<number>;
+  rest: ExerciseControl<number>;
+  weight: ExerciseControl<number>;
+  observation: ExerciseControl<string>;
+};
