ff07cf16989d61b9325144b8f515a54da62341f9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-29 22:59:40 -0300

    feat: implement the method to send exercises data to parent component

--- src/app/features/training/pages/create-training/components/exercise-modal/exercise-modal.component.ts
+++ src/app/features/training/pages/create-training/components/exercise-modal/exercise-modal.component.ts
@@ -1,9 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonItemGroup, IonModal, IonRow, IonTextarea, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonModal, IonRow, IonTextarea, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { chevronBackOutline } from 'ionicons/icons';
-import { ExercisesGroup } from '../../@types/exercises-group';
+import { Exercises, ExercisesGroup } from '../../@types/exercises';
 
 @Component({
   selector: 'app-exercise-modal',
@@ -16,38 +16,45 @@ import { ExercisesGroup } from '../../@types/exercises-group';
     IonButton,
     IonTitle,
     IonContent,
-    IonItem,
     IonInput,
     IonGrid,
     IonRow,
     IonCol,
     IonIcon,
-    IonItemGroup,
     IonTextarea,
+    ReactiveFormsModule,
   ],
 })
 export class ExerciseModalComponent {
-  @Input() addExercise = { open: false, divisionIndex: 0 };
-  @Output() addExerciseChange = new EventEmitter<{
-    open: boolean;
-    divisionIndex: number;
-  }>();
-  
-  exercises = new FormGroup<ExercisesGroup>({
-    uuid: new FormControl(''),
-    title: new FormControl(''),
-    sets: new FormControl(0),
-    reps: new FormControl(0),
-    rest: new FormControl(0),
-    weight: new FormControl(0),
-    observation: new FormControl(''),
-  })
+  @Input() isOpen = false;
+  @Output() isOpenChange = new EventEmitter<boolean>();
+  @Output() exerciseAdded = new EventEmitter<Exercises>();
+
+  exercisesForm = new FormGroup<ExercisesGroup>({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    sets: new FormControl(0, [Validators.required, Validators.min(1)]),
+    reps: new FormControl(0, [Validators.required, Validators.min(1)]),
+    rest: new FormControl(0, [Validators.required, Validators.min(1)]),
+    weight: new FormControl(),
+    observation: new FormControl(),
+  });
 
   constructor() {
     addIcons({ chevronBackOutline });
   }
 
-  onCloseModal(divisionIndex: number) {
-    this.addExerciseChange.emit({ open: false, divisionIndex });
+  onCloseModal() {
+    this.isOpenChange.emit(false);
+    this.exercisesForm.reset();
+  }
+
+  onAddExercise() {
+    if (this.exercisesForm.invalid) {
+      this.exercisesForm.markAllAsTouched();
+      return;
+    }
+
+    this.exerciseAdded.emit(this.exercisesForm.getRawValue() as Exercises);
+    this.exercisesForm.reset();
   }
 }
