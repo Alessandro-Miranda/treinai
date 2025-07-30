288d8442d448a135b0a07f2d78d6fad19ab1cd02
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-29 22:33:07 -0300

    refactor: remove exercises property from base object

--- src/app/features/training/pages/create-training/@types/add-exercise.ts
+++ src/app/features/training/pages/create-training/@types/add-exercise.ts
@@ -1,8 +1,4 @@
-import { FormArray, FormGroup } from "@angular/forms";
-import { ExercisesGroup } from "./exercises-group";
-
 export type AddExercise = {
   open: boolean;
   divisionIndex: number;
-  exercises: FormArray<FormGroup<ExercisesGroup>>;
 };
