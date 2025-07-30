cdfead70739c98d94314d263257fcff353980de6
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-29 22:41:28 -0300

    refactor: create Mapped types to improve types definitions of exercises

---
+++ src/app/features/training/pages/create-training/@types/exercises.ts
@@ -0,0 +1,21 @@
+import { FormControl } from "@angular/forms";
+
+type ExerciseControl<T> = {
+  [Property in keyof T]: FormControl<T[Property] | null>;
+};
+
+export type Exercises = {
+  name: string;
+  sets: number;
+  reps: number;
+  rest: number;
+  weight?: number;
+  observation?: string;
+};
+
+export type ExercisesGroup = ExerciseControl<Exercises>
+
+export type ExerciseModalState = {
+  open: boolean;
+  divisionIndex: number;
+};
