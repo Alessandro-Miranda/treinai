ac78b71aa341d135ea2bbde65e140c309bb53122
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-31 10:53:37 -0300

    refactor: remove user structure creation to keep Single Responsability principle

--- src/app/core/firebase/firebase-user.service.ts
+++ src/app/core/firebase/firebase-user.service.ts
@@ -1,25 +1,17 @@
 import { inject, Injectable } from "@angular/core";
-import { User } from "@angular/fire/auth";
-import { doc, Firestore, getDoc, serverTimestamp, setDoc } from "@angular/fire/firestore";
+import { doc, Firestore, getDoc, setDoc } from "@angular/fire/firestore";
 import { UserData } from "../interfaces/user.inteface";
 
 @Injectable({ providedIn: 'root' })
 export class FirebaseUserService {
   private firestore = inject(Firestore);
   
-  async createUser(user: User): Promise<void> {
+  async createUser(user: UserData): Promise<void> {
     const userRef = doc(this.firestore, 'users', user.uid);
     const userSnapshot = await getDoc(userRef);
 
     if (userSnapshot.exists()) return;
 
-    const userData: UserData = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      createdAt: serverTimestamp(),
-    };
-
-    return await setDoc(userRef, userData);
+    return await setDoc(userRef, user);
   }
 }
