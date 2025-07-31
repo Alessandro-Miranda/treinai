1aaf27178a790eef8be2aa0895481c83ab240b6c
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-31 15:01:46 -0300

    feat: add uuid and userId to workout interface

--- src/app/core/interfaces/workout.interface.ts
+++ src/app/core/interfaces/workout.interface.ts
@@ -1,5 +1,17 @@
 import { FormArray, FormControl } from "@angular/forms";
-import { DivisionGroup } from "@Features/training/pages/create-training/@types/exercises";
+import { DivisionGroup, Exercises } from "@Features/training/pages/create-training/@types/exercises";
+
+type Divisions = {
+  title: string;
+  exercises: Exercises[];
+}
+
+export interface Workout {
+  uuid: string;
+  name: string;
+  duration: string;
+  divisions: Divisions[];
+}
 
 export interface WorkoutData {
   name: FormControl<string | null>;
