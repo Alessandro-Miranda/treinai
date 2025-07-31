92af9d626d7a1dfd9c42314bf6e8d6cee988460a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-31 10:54:15 -0300

    refactor: add user model to createUser

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,12 +1,18 @@
-import { inject, Injectable } from "@angular/core";
-import { User } from "@angular/fire/auth";
-import { FirebaseUserService } from "../firebase/firebase-user.service";
+import { inject, Injectable } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { serverTimestamp } from '@angular/fire/firestore';
+import { FirebaseUserService } from '../firebase/firebase-user.service';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private firebaseUserService = inject(FirebaseUserService);
 
   async createIfNotExists(user: User): Promise<void> {
-    return this.firebaseUserService.createUser(user);
+    return this.firebaseUserService.createUser({
+      uid: user.uid,
+      displayName: user.displayName,
+      email: user.email,
+      createdAt: serverTimestamp(),
+    });
   }
 }
