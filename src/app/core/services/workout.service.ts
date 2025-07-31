2d43f35a02a4882f9537199dd49f44f343016d80
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-07-31 20:41:07 -0300

    refactor: update import of auth service

--- src/app/core/services/workout.service.ts
+++ src/app/core/services/workout.service.ts
@@ -1,12 +1,12 @@
 import { inject, Injectable } from "@angular/core";
 import { FormGroup } from "@angular/forms";
-import { FirebaseAuthService } from "../firebase/firebase-auth.service";
+import { AuthService } from "../firebase/auth.service";
 import { FirestoreService } from "../firebase/firestore.service";
 import { Workout, WorkoutData } from "../interfaces/workout.interface";
 
 @Injectable({ providedIn: 'root' })
 export class WorkoutService {
-  private firebaseAuthService = inject(FirebaseAuthService);
+  private authService = inject(AuthService);
   private firestoreService = inject(FirestoreService);
 
   async createWorkout(workoutForm: FormGroup<WorkoutData>) {
@@ -15,7 +15,7 @@ export class WorkoutService {
 
     return this.firestoreService.create<Workout>(`workouts/${workoutId}`, {
       id: workoutId,
-      userId: this.firebaseAuthService.getCurrentUser()!.uid,
+      userId: this.authService.getCurrentUser()!.uid,
       ...workout,
     });
   }
