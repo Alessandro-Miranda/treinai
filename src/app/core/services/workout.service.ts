f020d6bc6a693a3bba02d2ada18cee446cedab23
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-01 21:26:45 -0300

    refactor: change listWorkouts params asign

--- src/app/core/services/workout.service.ts
+++ src/app/core/services/workout.service.ts
@@ -1,9 +1,10 @@
-import { inject, Injectable } from "@angular/core";
-import { serverTimestamp } from "@angular/fire/firestore";
-import { FormGroup } from "@angular/forms";
-import { AuthService } from "../firebase/auth.service";
-import { FirestoreService } from "../firebase/firestore.service";
-import { Workout, WorkoutData } from "../interfaces/workout.interface";
+import { inject, Injectable } from '@angular/core';
+import { serverTimestamp } from '@angular/fire/firestore';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService } from '../firebase/auth.service';
+import { FirestoreService } from '../firebase/firestore.service';
+import { Workout, WorkoutData } from '../interfaces/workout.interface';
 
 @Injectable({ providedIn: 'root' })
 export class WorkoutService {
@@ -11,14 +12,31 @@ export class WorkoutService {
   private firestoreService = inject(FirestoreService);
 
   async createWorkout(workoutForm: FormGroup<WorkoutData>) {
-    const workout = workoutForm.getRawValue() as Omit<Workout, "id" | "userId" | "createdAt">;
+    const workout = workoutForm.getRawValue() as Omit<
+      Workout,
+      'id' | 'userId' | 'createdAt'
+    >;
     const workoutId = crypto.randomUUID();
 
-    return this.firestoreService.create<Workout>(`workouts/${workoutId}`, {
-      id: workoutId,
-      userId: this.authService.getCurrentUser()!.uid,
-      ...workout,
-      createdAt: serverTimestamp()
+    return this.firestoreService.create<Workout>({
+      path: `workouts/${workoutId}`,
+      data: {
+        id: workoutId,
+        userId: this.authService.getCurrentUser()!.uid,
+        ...workout,
+        createdAt: serverTimestamp(),
+      },
+    });
+  }
+
+  listWorkouts(): Observable<Workout[]> {
+    return this.firestoreService.findMany<Workout>({
+      path: 'workouts',
+      where: {
+        fieldPath: 'userId',
+        operation: '==',
+        value: this.authService.getCurrentUser()!.uid
+      }
     });
   }
 }
