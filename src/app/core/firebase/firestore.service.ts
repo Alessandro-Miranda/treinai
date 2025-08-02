3530b0724bfd09f31d3a2c75bc4345ed0820a0fc
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-01 21:36:38 -0300

    feat(refactor): implement method findMany and implement consistent method signature

--- src/app/core/firebase/firestore.service.ts
+++ src/app/core/firebase/firestore.service.ts
@@ -1,41 +1,77 @@
 import { inject, Injectable } from '@angular/core';
 import {
+  collection,
+  collectionData,
+  CollectionReference,
   doc,
   DocumentData,
   DocumentReference,
   DocumentSnapshot,
   Firestore,
   getDoc,
+  query,
   setDoc,
+  where,
+  WhereFilterOp,
   WithFieldValue,
 } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { FirebaseContext } from './firebase-context.helper';
+
+type QueryPath = {
+  path: string;
+};
+
+type CreateParams<T> = QueryPath & {
+  data: WithFieldValue<{ [Property in keyof T]: T[Property] }>;
+};
+
+type ReadParams<T> = QueryPath & {
+  where: {
+    fieldPath: Extract<keyof T, string>;
+    operation: WhereFilterOp;
+    value: T[keyof T];
+  };
+};
 
 @Injectable({ providedIn: 'root' })
 export class FirestoreService {
   private firestore = inject(Firestore);
   private firebaseContext = inject(FirebaseContext);
 
-  async create<T>(
-    path: string,
-    data: WithFieldValue<{ [Property in keyof T]: T[Property] }>
-  ): Promise<void> {
+  async create<T>({ data, path }: CreateParams<T>): Promise<void> {
     return this.firebaseContext.runInContext<void>(() =>
-      setDoc(this.getRef(path), data)
+      setDoc(this.getRef({ path }), data)
     );
   }
 
-  async read(path: string): Promise<DocumentSnapshot> {
+  async findDoc({ path }: QueryPath): Promise<DocumentSnapshot> {
     return this.firebaseContext.runInContext<DocumentSnapshot>(() =>
-      getDoc(this.getRef(path))
+      getDoc(this.getRef({ path }))
     );
   }
 
-  getRef(path: string): DocumentReference<DocumentData> {
+  findMany<T>({ path, where: filter }: ReadParams<T>): Observable<T[]> {
+    const collection = this.getCollection({ path });
+    const q = query(
+      collection,
+      where(filter.fieldPath, filter.operation, filter.value)
+    );
+    
+    return collectionData(q) as Observable<T[]>;
+  }
+
+  getRef({ path }: QueryPath): DocumentReference<DocumentData> {
     const docRef = this.firebaseContext.runInContext<
       DocumentReference<DocumentData>
     >(() => doc(this.firestore, path)) as DocumentReference<DocumentData>;
 
     return docRef;
+  }
+
+  getCollection({ path }: QueryPath): CollectionReference {
+    return this.firebaseContext.runInContext<CollectionReference<DocumentData>>(() =>
+      collection(this.firestore, path)
+    ) as CollectionReference<DocumentData>;
   }
 }
