921a244c6db542592db70477b34276b2246d241b
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-03 22:34:41 -0300

    feat: init training-details feat

---
+++ src/app/features/training/pages/training-details/training-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-detail',
+  templateUrl: './training-details.component.html',
+  styleUrls: ['./training-details.component.scss'],
+})
+export class TrainingDetailComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
