fc89f4f1ce1fb4af1ba99555c0dc5265ac799791
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-04 17:27:46 -0300

    feat: add imports used to initial style

--- src/app/features/training/pages/training-details/training-details.component.ts
+++ src/app/features/training/pages/training-details/training-details.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonIcon,
+  IonRow,
+  IonText,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { calendarOutline, chevronBackOutline } from 'ionicons/icons';
+import { Observable } from 'rxjs';
+import { Training } from 'src/app/core/interfaces/training.interface';
+import { TrainingService } from 'src/app/core/services/training.service';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
 
 @Component({
   selector: 'app-training-detail',
   templateUrl: './training-details.component.html',
   styleUrls: ['./training-details.component.scss'],
+  imports: [
+    AsyncPipe,
+    IonContent,
+    HeaderComponent,
+    IonText,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+  ],
 })
-export class TrainingDetailComponent  implements OnInit {
+export class TrainingDetailsComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private trainingService = inject(TrainingService);
+  private trainingId!: string;
+  training$!: Observable<Training>;
 
-  constructor() { }
+  constructor() {
+    addIcons({ chevronBackOutline, calendarOutline });
+  }
 
-  ngOnInit() {}
-
+  async ngOnInit() {
+    this.trainingId = this.route.snapshot.params['id'];
+    this.training$ = this.trainingService.findTrainingById(this.trainingId);
+  }
 }
