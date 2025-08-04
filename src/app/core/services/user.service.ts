93d6dee0cb4d6e15a599e07f0da3e65a389d8d37
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-04 15:51:21 -0300

    change method to use an observable

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { User } from '@angular/fire/auth';
 import { serverTimestamp } from '@angular/fire/firestore';
+import { firstValueFrom } from 'rxjs';
 import { FirestoreService } from '../firebase/firestore.service';
 import { UserData } from '../interfaces/user.inteface';
 
@@ -10,28 +11,19 @@ export class UserService {
 
   async createIfNotExists(user: User): Promise<void> {
     const collectionpath = this.getCollectionPath(user);
-<<<<<<< HEAD
     const userSnapshot = await firstValueFrom(
       this.firestoreService.findDoc<DocumentSnapshot>({
         path: collectionpath,
       })
     );
-=======
-    const userSnapshot = await this.firestoreService.findDoc({
-      path: collectionpath,
-    });
->>>>>>> f940025f9c41c27def47a4e1364af005251951d0
 
     if (userSnapshot.exists()) return;
 
-    return await this.firestoreService.create<UserData>({
-      path: collectionpath,
-      data: {
-        uid: user.uid,
-        displayName: user.displayName,
-        email: user.email,
-        createdAt: serverTimestamp(),
-      },
+    return await this.firestoreService.create<UserData>(collectionpath, {
+      uid: user.uid,
+      displayName: user.displayName,
+      email: user.email,
+      createdAt: serverTimestamp()
     });
   }
 
