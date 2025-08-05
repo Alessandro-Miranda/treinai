19743f3fe4a760ad601414383727b6de7f85fc17
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-05 11:05:36 -0300

    feat: add exercises-card-detail component

--- src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
+++ src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, forwardRef } from "@angular/core";
 import { Exercises } from "@Features/training/pages/create-training/@types/exercises";
 import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from "@ionic/angular/standalone";
 import { CardComponent } from "src/app/shared/card/card.component";
@@ -9,8 +9,25 @@ import { CardHeaderComponent } from "src/app/shared/card/components/card-header/
   selector: 'app-exercises-card',
   templateUrl: './exercises-card.component.html',
   styleUrl: './exercises-card.component.scss',
-  imports: [CardComponent, CardHeaderComponent, CardContentComponent, IonGrid, IonRow, IonCol, IonButton, IonIcon]
+  imports: [CardComponent, CardHeaderComponent, CardContentComponent, IonGrid, IonRow, IonCol, IonButton, IonIcon, forwardRef(() => ExerciseCardDetailComponent)]
 })
 export class ExercisesCardComponent {
   @Input({ required: true }) exercise!: Exercises
+}
+
+@Component({
+  selector: 'app-exercises-card-detail',
+  template: `
+    <ion-col size="auto">
+      <p>{{ label }}</p>
+      <p>
+        {{ value }}
+      </p>
+    </ion-col>
+  `,
+  imports: [IonCol],
+})
+export class ExerciseCardDetailComponent {
+  @Input({ required: true }) label!: string;
+  @Input({ required: true }) value?: string | number;
 }
