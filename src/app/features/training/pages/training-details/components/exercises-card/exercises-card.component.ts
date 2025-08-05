cef5689f60380df11d8008ae1e7d196144ac92f9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-05 11:07:52 -0300

    feat(refactor): create dedicated card detail component

--- src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
+++ src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
@@ -1,33 +1,31 @@
-import { Component, Input, forwardRef } from "@angular/core";
-import { Exercises } from "@Features/training/pages/create-training/@types/exercises";
-import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from "@ionic/angular/standalone";
-import { CardComponent } from "src/app/shared/card/card.component";
-import { CardContentComponent } from "src/app/shared/card/components/card-content/card-content.component";
-import { CardHeaderComponent } from "src/app/shared/card/components/card-header/card-header.component";
+import { Component, Input } from '@angular/core';
+import { Exercises } from '@Features/training/pages/create-training/@types/exercises';
+import {
+  IonButton,
+  IonGrid,
+  IonIcon,
+  IonRow
+} from '@ionic/angular/standalone';
+import { CardComponent } from 'src/app/shared/card/card.component';
+import { CardContentComponent } from 'src/app/shared/card/components/card-content/card-content.component';
+import { CardHeaderComponent } from 'src/app/shared/card/components/card-header/card-header.component';
+import { ExerciseCardDetailComponent } from "./exercises-card-detail.component";
 
 @Component({
   selector: 'app-exercises-card',
   templateUrl: './exercises-card.component.html',
   styleUrl: './exercises-card.component.scss',
-  imports: [CardComponent, CardHeaderComponent, CardContentComponent, IonGrid, IonRow, IonCol, IonButton, IonIcon, forwardRef(() => ExerciseCardDetailComponent)]
+  imports: [
+    CardComponent,
+    CardHeaderComponent,
+    CardContentComponent,
+    IonGrid,
+    IonRow,
+    IonButton,
+    IonIcon,
+    ExerciseCardDetailComponent
+],
 })
 export class ExercisesCardComponent {
-  @Input({ required: true }) exercise!: Exercises
-}
-
-@Component({
-  selector: 'app-exercises-card-detail',
-  template: `
-    <ion-col size="auto">
-      <p>{{ label }}</p>
-      <p>
-        {{ value }}
-      </p>
-    </ion-col>
-  `,
-  imports: [IonCol],
-})
-export class ExerciseCardDetailComponent {
-  @Input({ required: true }) label!: string;
-  @Input({ required: true }) value?: string | number;
+  @Input({ required: true }) exercise!: Exercises;
 }
