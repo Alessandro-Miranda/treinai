5d1542d83ac40b4fadef65a066cf58870c8d13a3
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-05 10:41:48 -0300

    feat(refactor): migrate imports and methods to dedicate training card component

--- src/app/features/training/pages/training-details/training-details.component.ts
+++ src/app/features/training/pages/training-details/training-details.component.ts
@@ -1,15 +1,15 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonIcon, IonRow, IonText } from '@ionic/angular/standalone';
+import { IonCol, IonContent, IonGrid, IonIcon, IonRow, IonText } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { calendarOutline, chevronBackOutline } from 'ionicons/icons';
 import { Observable } from 'rxjs';
-import { Divisions, Training } from 'src/app/core/interfaces/training.interface';
+import { Training } from 'src/app/core/interfaces/training.interface';
 import { TrainingService } from 'src/app/core/services/training.service';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { ExercisesListModalComponent } from "./components/exercises-list-modal/exercises-list-modal.component";
-import { ExercisesListModalService } from './components/exercises-list-modal/exercises-list-modal.service';
+import { TrainingDetailsCardComponent } from "./components/training-details-card/training-details-card.component";
 
 @Component({
   selector: 'app-training-details',
@@ -24,18 +24,13 @@ import { ExercisesListModalService } from './components/exercises-list-modal/exe
     IonGrid,
     IonRow,
     IonCol,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardTitle,
-    IonButton,
-    ExercisesListModalComponent
+    ExercisesListModalComponent,
+    TrainingDetailsCardComponent
 ],
 })
 export class TrainingDetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private trainingService = inject(TrainingService);
-  private exercisesListService = inject(ExercisesListModalService);
   private trainingId!: string;
   training$!: Observable<Training>;
 
@@ -46,9 +41,5 @@ export class TrainingDetailsComponent implements OnInit {
   async ngOnInit() {
     this.trainingId = this.route.snapshot.params['id'];
     this.training$ = this.trainingService.findTrainingById(this.trainingId);
-  }
-
-  openExerciseList(exerciseDivision: Divisions) {
-    this.exercisesListService.open(exerciseDivision);
   }
 }
