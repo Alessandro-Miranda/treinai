d179c61387fdc7df8b207d96944ac48f8c08aad1
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-05 15:20:03 -0300

    feat: add id to card to create an popover trigger

--- src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
+++ src/app/features/training/pages/training-details/components/exercises-card/exercises-card.component.ts
@@ -9,6 +9,7 @@ import {
 import { CardComponent } from 'src/app/shared/card/card.component';
 import { CardContentComponent } from 'src/app/shared/card/components/card-content/card-content.component';
 import { CardHeaderComponent } from 'src/app/shared/card/components/card-header/card-header.component';
+import { PopoverComponent } from "src/app/shared/popover/popover.component";
 import { ExerciseCardDetailComponent } from "./exercises-card-detail.component";
 
 @Component({
@@ -23,9 +24,11 @@ import { ExerciseCardDetailComponent } from "./exercises-card-detail.component";
     IonRow,
     IonButton,
     IonIcon,
-    ExerciseCardDetailComponent
+    ExerciseCardDetailComponent,
+    PopoverComponent
 ],
 })
 export class ExercisesCardComponent {
   @Input({ required: true }) exercise!: Exercises;
+  @Input({ required: true }) id!: string | number;
 }
