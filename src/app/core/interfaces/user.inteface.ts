d58c3d429e44fad5cc70b9fcbf77d27d2a06ffa9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-06 17:09:26 -0300

    refactor: update user data type

--- src/app/core/interfaces/user.inteface.ts
+++ src/app/core/interfaces/user.inteface.ts
@@ -1,8 +1,6 @@
-import { FieldValue } from "@angular/fire/firestore";
-
 export interface UserData {
   uid: string,
   email: string | null;
   displayName: string | null;
-  createdAt: FieldValue
+  createdAt: number;
 }
