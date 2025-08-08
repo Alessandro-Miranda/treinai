cc0e81fa6a2306cb06cec5a7a223b5c68a90ca20
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-07 21:34:41 -0300

    refactor: implement firestore strategy

--- src/app/core/firebase/firestore.service.ts
+++ src/app/core/firebase/firestore.service.ts
@@ -1,74 +1,29 @@
 import { inject, Injectable } from '@angular/core';
-import {
-  collection,
-  collectionData,
-  CollectionReference,
-  doc,
-  docSnapshots,
-  DocumentData,
-  DocumentReference,
-  Firestore,
-  query,
-  setDoc,
-  where,
-  WhereFilterOp,
-  WithFieldValue
-} from '@angular/fire/firestore';
+import { Capacitor } from '@capacitor/core';
 import { Observable } from 'rxjs';
-import { FirebaseContext } from './decorators/firebase-context';
-import { RunInFirebaseContext } from './decorators/run-in-context';
+import { CreateParams, FirestoreStrategyInterface, QueryPath, ReadParams } from './interfaces/firestore.interface';
+import { NativeFirestoreStrategy } from './strategies/native-firestore.strategy';
+import { WebFirestoreStrategy } from './strategies/web-firestore.strategy';
 
-type QueryPath = {
-  path: string;
-};
-
-type CreateParams<T> = QueryPath & {
-  data: WithFieldValue<{ [Property in keyof T]: T[Property] }>;
-};
-
-type ReadParams<T> = QueryPath & {
-  where: {
-    fieldPath: Extract<keyof T, string>;
-    operation: WhereFilterOp;
-    value: T[keyof T];
-  };
-};
-
-@RunInFirebaseContext
 @Injectable({ providedIn: 'root' })
-export class FirestoreService extends FirebaseContext {
-  private firestore = inject(Firestore);
-
-  async create<T>({ data, path }: CreateParams<T>): Promise<void> {
-    return setDoc(this.getRef({ path }), data);
-  }
-
-  findDoc<T>({ path }: QueryPath): Observable<T> {
-    return docSnapshots(this.getRef({ path })) as Observable<T>;
-  }
-
-  findMany<T>({ path, where: filter }: ReadParams<T>): Observable<T[]> {
-    const collection = this.getCollection({ path });
-    const q = this.prepareWhereQuery({ collection, where: filter });
-
-    return collectionData(q) as Observable<T[]>;
-  }
-
-  getRef({ path }: QueryPath): DocumentReference<DocumentData> {
-    return doc(this.firestore, path);
-  }
-
-  getCollection({ path }: QueryPath): CollectionReference {
-    return collection(this.firestore, path);
-  }
+export class FirestoreService implements FirestoreStrategyInterface {
+  private webFirestore = inject(WebFirestoreStrategy);
+  private nativeFirestore = inject(NativeFirestoreStrategy);
+  private firestore: FirestoreStrategyInterface;
   
-  prepareWhereQuery({
-    collection,
-    where: filter,
-  }: Pick<ReadParams<any>, 'where'> & { collection: CollectionReference }) {
-    return query(
-      collection,
-      where(filter.fieldPath, filter.operation, filter.value)
-    );
+  constructor() {
+    this.firestore = Capacitor.isNativePlatform() ? this.nativeFirestore : this.webFirestore;
   }
+
+ create<T>(params: CreateParams<T>): Promise<void> {
+  return this.firestore.create(params);
+ }
+
+ findDoc<T>(params: QueryPath): Observable<T> {
+  return this.firestore.findDoc(params);
+ }
+
+ findMany<T>(params: ReadParams<T>): Observable<T[]> {
+  return this.firestore.findMany(params);
+ }
 }
