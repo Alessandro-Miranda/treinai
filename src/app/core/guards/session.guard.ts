ee87bebfde38e8c9d6a863e196b06171f3a825e8
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-07 23:46:12 -0300

    refactor: update guard to use AuthService

--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -1,22 +1,18 @@
 import { inject, Injectable } from "@angular/core";
-import { Auth } from "@angular/fire/auth";
 import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: 'root' })
 export class CanActivateSession implements CanActivate {
-  private auth = inject(Auth);
+  private auth = inject(AuthService);
   private router = inject(Router);
 
   async canActivate(): Promise<boolean> {
-    return new Promise((resolve) => {
-      this.auth.onAuthStateChanged(user => {
-        if (user) {
-          resolve(true);
-        } else {
-          this.router.navigate(['/auth']);
-          resolve(false);
-        }
-      });
-    });
+    const user = await this.auth.getCurrentUser();
+
+    if (user) return true;
+
+    this.router.navigate(['/auth']);
+    return false;
   }
 }
