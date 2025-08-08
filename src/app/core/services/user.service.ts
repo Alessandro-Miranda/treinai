afbe66d0df1155ddfdb2cfebf4f69653cdb344fa
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-07 22:12:30 -0300

    refactor: update use of findDoc method

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,6 +1,5 @@
 import { inject, Injectable } from '@angular/core';
 import { DocumentData } from '@angular/fire/firestore';
-import { firstValueFrom } from 'rxjs';
 import { UserData } from '../auth/interfaces/user-data.interace';
 import { FirestoreService } from '../firebase/firestore.service';
 
@@ -10,11 +9,7 @@ export class UserService {
 
   async createIfNotExists(user: UserData): Promise<void> {
     const collectionpath = this.getCollectionPath(user);
-    const userSnapshot = await firstValueFrom(
-      this.firestoreService.findDoc<DocumentData>({
-        path: collectionpath,
-      })
-    );
+    const userSnapshot = await this.firestoreService.findDoc<DocumentData>({ path: collectionpath });
 
     if (userSnapshot) return;
 
