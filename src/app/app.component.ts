5b9603b6148d21fd39338dae6f0c66fb8a36e1c7
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-08 10:30:43 -0300

    fix: add StatusBar config to avoid overlap in heaer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+StatusBar.setStyle({ style: Style.Default })
+StatusBar.setOverlaysWebView({ overlay: false });
 
 @Component({
   selector: 'app-root',
