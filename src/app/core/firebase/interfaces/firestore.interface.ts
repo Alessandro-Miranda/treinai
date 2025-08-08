8009fe4e131795910ccfe133755802b036a2a93d
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-08-07 21:29:23 -0300

    refactor: create the contract implemented by firestore strategies

---
+++ src/app/core/firebase/interfaces/firestore.interface.ts
@@ -0,0 +1,24 @@
+import { WhereFilterOp, WithFieldValue } from "firebase/firestore";
+import { Observable } from "rxjs";
+
+export type QueryPath = {
+  path: string;
+};
+
+export type CreateParams<T> = QueryPath & {
+  data: WithFieldValue<{ [Property in keyof T]: T[Property] }>;
+};
+
+export type ReadParams<T> = QueryPath & {
+  where: {
+    fieldPath: Extract<keyof T, string>;
+    operation: WhereFilterOp;
+    value: T[keyof T];
+  };
+};
+
+export interface FirestoreStrategyInterface {
+  create: <T>(params: CreateParams<T>) => Promise<void>;
+  findDoc: <T>(params: QueryPath) => Observable<T>;
+  findMany: <T>(params: ReadParams<T>) => Observable<T[]>;
+}
