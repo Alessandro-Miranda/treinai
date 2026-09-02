5b5f3b0d8f273c17a6be61206d92cf98788fdc33
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2026-09-01 22:47:58 -0300

    feat: finish login page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import {
+  arrowForwardOutline,
+  lockClosedOutline,
+  logoGoogle,
+  mailOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +14,12 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      mailOutline,
+      lockClosedOutline,
+      logoGoogle,
+      arrowForwardOutline,
+    });
+  }
 }
