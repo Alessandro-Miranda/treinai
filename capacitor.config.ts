c724527bfc262da834d5b25c74b49fa2e1799059
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2026-09-17 23:42:15 -0300

    chore: adicionar configuração do @capacitor-firebas/ authenticator

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.com.treinai',
   appName: 'treinai',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    FirebaseAuthentication: {
+      providers: ['google.com']
+    }
+  }
 };
 
 export default config;
